1ddd22c489bab6361e4d281b972d228dfbab8f12
Andrey <irotaev@gmail.com>
2018-06-18 18:06:57 +0300

    Add change location functionality to draggble element

--- src/app/directive/am-draggable.directive.ts
+++ src/app/directive/am-draggable.directive.ts
@@ -7,17 +7,39 @@ import {NgDragDropService} from 'ng-drag-drop/src/services/ng-drag-drop.service'
 })
 export class AmDraggableDirective extends Draggable {
 
+    IsChangeLocation = true;
+
     constructor(el: ElementRef, renderer: Renderer2, ng2DragDropService: NgDragDropService, zone: NgZone) {
         super(el, renderer, ng2DragDropService, zone);
 
-        this.onDragStart.subscribe(event => {
-            console.log(event);
-            this.el.nativeElement.style.opacity = 0;
-        });
+        // this.clientX = this.el.nativeElement.style.left;
+        // this.clientY = this.el.nativeElement.style.top;
 
-        this.onDragEnd.subscribe(event => {
-            console.log(event);
-            this.el.nativeElement.style.opacity = 1;
-        });
+        this.onDragStart.subscribe(event => this.fillStartDragState(event));
+        this.onDragEnd.subscribe(event => this.changeLocation(event));
     }
+
+    //#region Change current element location
+
+    private _lastDragStartEvent?: DragEvent;
+
+    private fillStartDragState(event: DragEvent) {
+        this._lastDragStartEvent = event;
+    }
+
+    private changeLocation(event: DragEvent) {
+        if (!this.IsChangeLocation) {
+            return;
+        }
+
+        const deltaX = event.clientX - this._lastDragStartEvent.clientX;
+        const deltaY = event.clientY - this._lastDragStartEvent.clientY;
+
+        const style = window.getComputedStyle(this.el.nativeElement, null);
+
+        this.el.nativeElement.style.left = parseInt(style.left, 10) + deltaX + 'px';
+        this.el.nativeElement.style.top = parseInt(style.top, 10) + deltaY + 'px';
+    }
+
+    //#endregion
 }

eab94e480236880704e9a2355f3e31c6592f57ed
irotaev <irotaev@gmaill.com>
2019-09-06 18:36:29 +0300

    \dev

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './sci/app.module';
 import { environment } from './environments/environment';
+import {hmrBootstrap} from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (environment.hmr) {
+  // @ts-ignore
+  if (module.hot) {
+    // @ts-ignore
+    module.hot.accept();
+    hmrBootstrap(module, bootstrap);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap().catch(err => console.log(err));
+}
